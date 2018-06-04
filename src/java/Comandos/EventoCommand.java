366af6fa5210253074a561398fcba00c4dcc4b7a
Rian Alves <Rian Alves@RIAN.local>
2018-06-04 17:31:05 -0300

    >Alteração na classe EventoCommand

--- src/java/Comandos/EventoCommand.java
+++ src/java/Comandos/EventoCommand.java
@@ -1,6 +1,10 @@
 package Comandos;
 
+import DAOs.EventoDAO;
+import Modelo.Evento;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
@@ -8,11 +12,25 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class EventoCommand implements Comando{
+public class EventoCommand implements Comando {
 
     @Override
     public void exec(HttpServletRequest request, HttpServletResponse response) {
 
+        try {
+            EventoDAO dao = EventoDAO.getInstance();
+            List<Evento> eventos = dao.listAll();
+            RequestDispatcher dispacher = request.getRequestDispatcher("/WEB-INF/index.jsp");
+            request.setAttribute("eventos", eventos);
+            dispacher.forward(request, response);
+            
+       } catch (ServletException ex) {
+            Logger.getLogger(EventoCommand.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(EventoCommand.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(EventoCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
 }
